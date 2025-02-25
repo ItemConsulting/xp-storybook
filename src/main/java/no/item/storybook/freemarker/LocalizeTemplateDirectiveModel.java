cc6b05fdb4d3a1d9376105c6cd8b03bb60bb27a1
Tom Arild Jakobsen <tajakobsen@gmail.com>
2025-02-25 09:11:55 +0100

    Fix `localize` in Freemarker with multiple baseDirPaths (#29)
    Closes: #28

--- src/main/java/no/item/storybook/freemarker/LocalizeTemplateDirectiveModel.java
+++ src/main/java/no/item/storybook/freemarker/LocalizeTemplateDirectiveModel.java
@@ -4,6 +4,8 @@ import freemarker.core.Environment;
 import freemarker.template.TemplateDirectiveBody;
 import freemarker.template.TemplateDirectiveModel;
 import freemarker.template.TemplateModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,21 +13,26 @@ import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 public class LocalizeTemplateDirectiveModel implements TemplateDirectiveModel {
-  private final List<String> baseDirPaths;
+  private final static Logger log = LoggerFactory.getLogger(LocalizeTemplateDirectiveModel.class);
+  private final String baseDirPath;
 
-  public LocalizeTemplateDirectiveModel(List<String> baseDirPaths) {
-    this.baseDirPaths = baseDirPaths;
+  public LocalizeTemplateDirectiveModel(String baseDirPath) {
+    this.baseDirPath = baseDirPath;
   }
 
   @Override
   public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws IOException {
     try (Writer out = env.getOut()) {
+      if(baseDirPath == null) {
+        out.append("NOT_TRANSLATED");
+        return;
+      }
+
       ResourceBundle bundle = getResourceBundle(params);
       String key = params.get("key").toString();
 
@@ -40,18 +47,15 @@ public class LocalizeTemplateDirectiveModel implements TemplateDirectiveModel {
   private ResourceBundle getResourceBundle(Map params) {
     Locale locale = params.containsKey("locale") ? Locale.forLanguageTag(params.get("locale").toString()) : Locale.ROOT;
 
-    URL[] urls = baseDirPaths.stream()
-      .map(baseDirPath -> new File(baseDirPath + File.separator + "i18n"))
-      .map(file -> {
-        try {
-          return file.toURI().toURL();
-        } catch (MalformedURLException e) {
-          throw new RuntimeException(e);
-        }
-      }).toArray(URL[]::new);
+    File dir = new File(baseDirPath + File.separator + "i18n");
 
-    ClassLoader loader = new URLClassLoader(urls);
-
-    return ResourceBundle.getBundle("phrases", locale, loader);
+    try {
+      URL url = dir.toURI().toURL();
+      ClassLoader loader = new URLClassLoader(new URL[] {url});
+      return ResourceBundle.getBundle("phrases", locale, loader);
+    } catch (MalformedURLException e) {
+      log.error("Could not load resource bundle", e);
+      return ResourceBundle.getBundle("phrases", locale);
+    }
   }
 }
